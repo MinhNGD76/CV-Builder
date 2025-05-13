7e920ab069a1407c86321a8ed1eb45b2f26d0099
bunchangon711 <htkhoi7112003@gmail.com>
2025-05-13 11:03:06 +0700

    Update gateway communications

--- gateway/src/main.ts
+++ gateway/src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  // Add CORS configuration
+  app.enableCors({
+    origin: ['http://localhost:5173', 'http://frontend:5173'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+  
   await app.listen(3000);
 }
 bootstrap();
